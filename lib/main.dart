4791f808fb667a766d63f98d62f7bd4da963607a
DavidAvigdor1996 <157929267+DavidAvigdor1996@users.noreply.github.com>
2024-10-21 01:35:11 +0300

    Fix dependency w/ .env usage
    Fixed the app's previous inherit dependency on sentry, and correctly added the .env implementation

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:get_it/get_it.dart';
 import 'package:mazilon/iFx/service_locator.dart';
+import 'package:mazilon/util/sentry_service.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'util/Firebase/firebase_options.dart';
@@ -17,15 +19,21 @@ import 'package:mazilon/util/Form/checkbox_model.dart';
 import 'package:mazilon/util/Form/formPagePhoneModel.dart';
 import 'package:mazilon/initialForm/form.dart';
 import 'package:mazilon/util/userSyncFirebaseProvider.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 //testing:
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mazilon/disclaimerPage.dart';
 import 'package:mazilon/pages/SignIn_Pages/firstPage.dart';
 import 'package:firebase_database/firebase_database.dart';
 
-void main() async {
+List<String> checkboxCollectionNames = [
+  'PersonalPlan-DifficultEvents',
+  'PersonalPlan-MakeSafer',
+  'PersonalPlan-FeelBetter',
+  'PersonalPlan-Distractions',
+  // Add the new table name
+];
+Future<FirebaseApp> initializeApp() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -40,66 +48,13 @@ void main() async {
     options: DefaultFirebaseOptions
         .currentPlatform, // Use your custom FirebaseOptions
   );
+  return dbUsersApp;
+}
 
-  //adding checkbox collection for the form add here:
-  List<String> checkboxCollectionNames = [
-    'PersonalPlan-DifficultEvents',
-    'PersonalPlan-MakeSafer',
-    'PersonalPlan-FeelBetter',
-    'PersonalPlan-Distractions',
-    // Add the new table name
-  ];
-  await SentryFlutter.init(
-    (options) {
-      options.dsn = dotenv.env['SENTRY_DSN'] ?? '';
-      // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
-      // We recommend adjusting this value in production.
-      options.tracesSampleRate = 1.0;
-      // The sampling rate for profiling is relative to tracesSampleRate
-      // Setting to 1.0 will profile 100% of sampled transactions:
-      options.profilesSampleRate = 1.0;
-    },
-    appRunner: () => runApp(
-      MultiProvider(
-        providers: [
-          for (int i = 0; i < checkboxCollectionNames.length; i++)
-            // Initialize the checkbox models
-            ChangeNotifierProvider(
-              create: (context) => CheckboxModel(checkboxCollectionNames[i],
-                  checkboxCollectionNames[i], "", "", "", ""),
-            ),
-          // Initialize the phonePageData provider
-          ChangeNotifierProvider(
-            create: (context) => PhonePageData(
-                key: "PhonePage",
-                phoneNames: [],
-                phoneNumbers: [],
-                header: "", // Blank for unknown field
-                subTitle: "", // Blank for unknown field
-                midTitle: "", // Blank for unknown field
-                phoneNameTitle: "", // Blank for unknown field
-                phoneNumberTitle: "", // Blank for unknown field
-                savedPhoneNames: [], // Assuming empty list for unknown
-                savedPhoneNumbers: [], // Assuming empty list for unknown
-                phoneDescription: [] // Assuming empty list for unknown
-                )
-              ..loadItemsFromPrefs(), // Initialize phonePageData
-          ),
-          //REMOVE COMMENT ON FUTURE UPDATES FOR SYNC DEVICE FUNCTIONALITY
-          // Initialize the FirebaseAppProvider for dbUsersApp-REALTIME DB
-          ChangeNotifierProvider(
-              create: (context) => FirebaseAppProvider(dbUsersApp)),
-          // Initialize the APP information provider
-          ChangeNotifierProvider(create: (context) => AppInformation()),
-          // Initialize the User information provider
-          ChangeNotifierProvider(create: (context) => UserInformation()),
-        ],
-        child: MyApp(),
-      ), // Wrap your app
-    ),
-  );
-  /*
-  runApp(
+void main() async {
+  FirebaseApp dbUsersApp = await initializeApp();
+  SentryService sentryService = GetIt.instance<SentryService>();
+  await sentryService.initializeSentry(
     MultiProvider(
       providers: [
         for (int i = 0; i < checkboxCollectionNames.length; i++)
@@ -137,8 +92,6 @@ void main() async {
       child: MyApp(),
     ),
   );
-
-*/
 }
 
 class MyApp extends StatefulWidget {
@@ -153,13 +106,7 @@ class _MyAppState extends State<MyApp> {
   List<List<String>> collections = [];
   bool _isInitialized = false;
   // adding checkboxmodel for the form add here:
-  List<String> checkboxCollectionNames = [
-    'PersonalPlan-DifficultEvents',
-    'PersonalPlan-MakeSafer',
-    'PersonalPlan-FeelBetter',
-    'PersonalPlan-Distractions',
-    // Add the new table name
-  ];
+
   List<String> phonePageCollectionNames = [
     'PersonalPlan-PhonesPage',
   ];
@@ -172,9 +119,6 @@ class _MyAppState extends State<MyApp> {
   List<String> homeTitles = [];
 
   AppInformation appInfo = AppInformation();
-  void mySetState() {
-    setState(() {});
-  }
 
   bool hasFilled = false;
   void getHasFilled() async {
@@ -209,12 +153,6 @@ class _MyAppState extends State<MyApp> {
   void didChangeDependencies() {
     if (!_isInitialized) {
       phonePageData = Provider.of<PhonePageData>(context);
-      /* loadCollectionsFuture = loadCollections(
-        checkboxCollectionNames,
-        collections,
-        checkboxModels,
-        appInfo,
-      );*/
       _isInitialized = true;
     }
     super.didChangeDependencies();
