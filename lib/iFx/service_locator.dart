db6c75ddd9d919c922fc51849ca6d3e30ea0c599
DavidAvigdor1996 <157929267+DavidAvigdor1996@users.noreply.github.com>
2024-10-21 03:51:09 +0300

    testing basic share and download
    added basic share and download tests (that the buttons are working, nothing more)
    also organized the files for that functionality.

--- lib/iFx/service_locator.dart
+++ lib/iFx/service_locator.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:mazilon/pages/FeelGood/image_picker_service_impl.dart';
 import 'package:mazilon/pages/WellnessTools/VideoPlayerPageFactory.dart';
+import 'package:mazilon/util/PDF/PDF_service.dart';
 
 // Initialize GetIt instance
 final getIt = GetIt.instance;
@@ -11,4 +12,5 @@ void setupLocator() {
       () => VideoPlayerPageFactoryImpl());
   getIt.registerLazySingleton<ImagePickerService>(
       () => ImagePickerServiceImpl());
+  getIt.registerLazySingleton<PDFService>(() => PDFServiceImpl());
 }
