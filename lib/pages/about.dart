762494b703f12181e77543a71260ca6f20138ea1
DavidAvigdor1996 <davida1996@campus.technion.ac.il>
2024-11-21 22:00:36 +0200

    added version notation to about page(hardcoded)

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -102,6 +102,19 @@ class About extends StatelessWidget {
                       35),
                 ),
               ),
+              Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Directionality(
+                        textDirection: TextDirection.ltr,
+                        child: Text(
+                          "Mazilon App Version : 1.1.0",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal, fontSize: 20),
+                        )),
+                  )),
               Row(
                 mainAxisAlignment:
                     MainAxisAlignment.spaceEvenly, // Adjust alignment as needed
