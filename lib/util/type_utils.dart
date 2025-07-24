8e64adcfb573aabfa5ffcd9e5dfd0fcadc540969
DavidAvigdor1996 <davida1996@campus.technion.ac.il>
2025-07-24 11:53:53 +0300

    Update type_utils.dart

--- lib/util/type_utils.dart
+++ lib/util/type_utils.dart
@@ -1,10 +1,11 @@
 class TypeUtils {
+  /// cast a dynamic value to List<String>
   static List<String> castToStringList(dynamic value) {
     return (value as List<dynamic>?)?.cast<String>() ?? <String>[];
   }
 
-  /// Safely cast a dynamic value to String
-  /// Returns an empty string if the value is null
+  /// cast a dynamic value to String
+
   static String castToString(dynamic value) {
     return value?.toString() ?? '';
   }
