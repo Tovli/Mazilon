0cd8625fe4301a34b53267548ec62e4bc7d72ba9
DavidAvigdor1996 <davida1996@campus.technion.ac.il>
2026-01-07 14:21:26 +0200

    Testing chang

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,12 +12,8 @@ val newBuildDir: Directory =
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val root = project.projectDir.toPath().root
-    val mainRoot = rootProject.projectDir.toPath().root
-    if (root == mainRoot) {
-        val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-        project.layout.buildDirectory.value(newSubprojectBuildDir)
-    }
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
     project.evaluationDependsOn(":app")
