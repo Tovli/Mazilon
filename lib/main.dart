31b38f60af8905c68790aced3673b3fe9f09d522
DavidAvigdor1996 <davida1996@campus.technion.ac.il>
2026-01-21 20:15:22 +0200

    Fixed mixpanel not loading on web, and disclaimer bug
    Fixed mixpanel erroring on web by adding a line calling for it's script in the index.html file.
    fixed the issue that on refresh, even without the user confirming the disclaimer, it'd be confirmed anyway.

--- lib/main.dart
+++ lib/main.dart
@@ -214,8 +214,6 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
       PersistentMemoryService service = GetIt.instance<
           PersistentMemoryService>(); // Get the persistent memory service instance
 
-      await service.setItem(
-          "disclaimerConfirmed", PersistentMemoryType.Bool, true);
       var hasFilledValue =
           await service.getItem("hasFilled", PersistentMemoryType.Bool) ??
               false;
@@ -236,8 +234,7 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
 
       PersistentMemoryService service = GetIt.instance<
           PersistentMemoryService>(); // Get the persistent memory service instance
-      await service.setItem(
-          "disclaimerConfirmed", PersistentMemoryType.Bool, true);
+
       String? prefsLocale =
           await service.getItem('localeName', PersistentMemoryType.String);
 
